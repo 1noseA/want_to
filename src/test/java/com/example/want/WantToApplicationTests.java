258d3f155067b6cffb72b402da4976515fc67a61
1noseA <1noseA2L@gmail.com>
2021-03-28 19:26:36 +0900

    first commit

---
+++ src/test/java/com/example/want/WantToApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.want;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantToApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
